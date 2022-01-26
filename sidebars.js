c6daf5174cf6d25028d0107ff8c8433f07cb65e4
Mitchell <mitchellmorley@gmail.com>
2022-01-26 11:01:04 -0800

    figure out nested category sidebar for swiss and swiss pairings

--- sidebars.js
+++ sidebars.js
@@ -1,32 +1,45 @@
 module.exports = {
-  someSidebar: {
-    'Introduction': [
-      'welcome',
-      'stakeholders',
-      'concepts',
-    ],
-    'Format Reference Guides': [
-      'format-guides-overview', 
-      'format-single-elim', 
-      'format-double-elim', 
-      'format-round-robin',
-      'format-swiss',
-    ],
-    'Additional Thoughts': [
-      'format-combinations',
-      'go-beyond',
-    ],
-    'Quick Info': [
-      'cheat-sheet',
-      'comparisons',
-    ]
-  },
+  docs: [
+    {
+      type: 'category',
+      label: 'Introduction',
+      items: [
+        'welcome',
+        'stakeholders',
+        'concepts'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Format Reference Guides',
+      items: [
+        'format-guides-overview', 
+        'format-single-elim', 
+        'format-double-elim', 
+        'format-round-robin',
+        {
+          type: 'category',
+          label: 'Swiss',
+          link: {type: 'doc', id: 'format-swiss'},
+          items: ['swiss-pairings']
+        }
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Additional Thoughts',
+      items: [
+        'format-combinations',
+        'go-beyond',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Quick Info',
+      items: [
+        'cheat-sheet',
+        'comparisons',
+      ]
+    }
+  ]
 };
-
-//     'Format Concepts': ['inputs-and-outputs', 'glossary', 'new-frontier'],
-
-/* 
-      'format-swiss',
-      'format-gauntlet',
-      'format-ladder',
-*/
